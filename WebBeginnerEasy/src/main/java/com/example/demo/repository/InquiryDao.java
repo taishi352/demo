8a3ff2d74a521fc5eb6e49be8cee1223bc8c0e4f
owner <owner@192.168.2.121>
2021-01-08 14:40:49 +0900

    practice spring

---
+++ WebBeginnerEasy/src/main/java/com/example/demo/repository/InquiryDao.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Inquiry;
+
+public interface InquiryDao {
+	
+	void insertInquiry(Inquiry inquiry);
+	
+	int updateInquiry(Inquiry inquiry);
+	
+	List<Inquiry> getAll();
+}
